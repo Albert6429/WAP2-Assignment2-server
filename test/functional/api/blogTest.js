892263ca39ed5a52e0ae5d5684feff265e6cb57e
Yifan Gu <20086429@mail.wit.ie>
2019-11-03 21:20:40 +0000

    POST /posts test when the id is invalid

--- test/functional/api/blogTest.js
+++ test/functional/api/blogTest.js
@@ -443,5 +443,18 @@ describe("postTest", () => {
                     });
             });
         });
+        describe("when the id is invalid", () => {
+            it("should return the NOT found message", done => {
+                request(server)
+                    .get("/posts/11111")
+                    .set("Accept", "application/json")
+                    .expect("Content-Type", /json/)
+                    .expect(200)
+                    .end((err, res) => {
+                        expect(res.body.message).equals("The post does not exist");
+                        done(err);
+                    });
+            });
+        });
     });
 });
