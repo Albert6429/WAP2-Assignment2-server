4484393649a6ae262388b03022d2d79f161d6542
Yifan Gu <20086429@mail.wit.ie>
2019-11-03 21:09:13 +0000

    DELETE /deleteUser/:id when the id is invalid

--- test/functional/api/blogTest.js
+++ test/functional/api/blogTest.js
@@ -281,5 +281,19 @@ describe("userTest", () => {
                     });
             });
         });
+
+        describe("when the id is invalid", () => {
+            it("should return the NOT exist message", done => {
+                request(server)
+                    .delete("/deleteUser/jojo")
+                    .set("Accept", "application/json")
+                    .expect("Content-Type", /json/)
+                    .expect(200)
+                    .end((err, res) => {
+                        expect(res.body.message).equals("The id does not exist");
+                        done(err);
+                    });
+            });
+        });
     });
 });
